c5eadc4bd52094f960cf6d0fb7e43f8d9a208d18
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2023-09-15 15:53:55 -0700

    Reorganize application into more modules

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,7 +4,7 @@ fn spawn_app() -> String {
     // Use port 0 to get a random port from the OS.
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
